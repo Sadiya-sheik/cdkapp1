c3ad9b2a68491e4c848d561a00984af7061f002d
Sadiya-sheik <83219444+Sadiya-sheik@users.noreply.github.com>
2021-04-29 15:50:56 +0530

    Add files via upload

---
+++ functions/function.js
@@ -0,0 +1,17 @@
+export async function handler (event) {
+    console.log("request:", JSON.stringify(event));
+  
+    // return response back to upstream caller my change
+    return sendRes(200, "HELLLOOO");
+  }
+  
+  const sendRes = (status, body) => {
+    var response = {
+      statusCode: status,
+      headers: {
+        "Content-Type": "text/html",
+      },
+      body: body,
+    };
+    return response;
+  };
